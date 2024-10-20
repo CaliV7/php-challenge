4436258eae48e2190cfb34b937704f1f68b77a91
Calista VOLGET <calistavolget@gmail.com>
2024-10-20 14:35:54 +0200

    create a directory with a php file and a screenshot.png for challenge 7

--- challenge6.php
+++ challenge6.php
@@ -6,5 +6,7 @@ function writeSecretSentence(string $param1, string $param2):string{
 
 echo writeSecretSentence('le feu','la lune');
 
+?>
+
 // On a créé une fonction ayant deux paramètres typés et un retour typés.
 // On renvoit le résutat avec 'return' puis on l'affiche en dehors de la fonction. J'ai pris le feu et la lune comme exemple.
---
+++ challenge7/challenge7.php
@@ -0,0 +1,24 @@
+<?php
+
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Volget Calista" => [
+        "city" => "Paris",
+        "weapon" => "flamethrower"
+    ]
+]; // On a repris le tableau proposé dans l'énoncé et on y a ajouté une clé  qui contient un tableau associatif.
+
+var_dump($characters); // On cherche à afficher toutes les données du tableau. 
+
+?>
